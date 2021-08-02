dcc3bd0035a815b30ba8e24927727d139e6cf7d3
OLLIT.DEV <86409818+Gopporovoyatillo@users.noreply.github.com>
2021-08-03 01:38:04 +0500

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,14 @@
+
+		import java.util.Scanner;
+class test {
+public  static  void main(String  args[]) {
+ Scanner sc =new Scanner(System.in);
+ System.out.print("Ismini kiriting ");
+ String name = sc.nextLine();
+ System.out.print("Yoshini kiriting ");
+ int old = sc.nextInt();
+ System.out.print("Vaznini kiriting ");
+ double kg = sc.nextDouble();
+ System.out.println("Ismi:"+name+"\nYoshi: "+old+"\nVazni:"+kg);
+	}
+}
